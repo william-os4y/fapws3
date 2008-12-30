7d69f47b65199961884e01b9c90b215b809f1722
William.os4y <william.os4y@gmail.com>
2008-12-30 23:33:25 +0100

    Add TRACE command sample
    Thanks to the last commit, it's becomes possible to extand Fapws
    by adding HTTP command. This sample show it with the HTTP TRACE command

---
+++ sample/http_cmd/run.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import fapws._evwsgi as evwsgi
+from fapws import base
+import time
+import sys
+from fapws.contrib import views, zip, log
+
+import trace
+
+base.supported_HTTP_command.append("TRACE")
+def start():
+    evwsgi.start("0.0.0.0", 8080)
+    evwsgi.set_base_module(base)
+    
+    @trace.Trace()
+    def hello(environ, start_response):
+        start_response('200 OK', [('Content-Type','text/html')])
+        return ["hello world!!"]
+
+    evwsgi.wsgi_cb(("/hello", hello))
+
+    evwsgi.set_debug(0)    
+    evwsgi.run()
+    
+
+if __name__=="__main__":
+    start()
---
+++ sample/http_cmd/trace.py
@@ -0,0 +1,19 @@
+
+import time
+import os.path
+import sys
+
+class Trace:
+    def __init__(self):
+        self.pp=4
+    def __call__(self, f):
+        def func(environ, start_response):
+            res=f(environ, start_response)
+            if environ['REQUEST_METHOD']=="TRACE":
+                res=[]
+                for elem in environ.keys():
+                    res.append("%s: %s\r\n" % (elem, str(environ[elem])))
+            return res
+
+        return func
+    
