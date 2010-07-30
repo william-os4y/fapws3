1a119459d3f823c1a6d89480d77ad90f4b7771c4
William.os4y <william.os4y@gmail.com>
2010-07-30 10:34:24 +0200

    fix on the defer test sample

--- sample/defer/test.py
+++ sample/defer/test.py
@@ -10,11 +10,11 @@ def toto(v):
     global count
     time.sleep(v)
     count+=1
-    print "defer sleep %s, counter %s" % (v,0)
+    print "defer sleep %s, counter %s, %s" % (v,count,evwsgi.defer_queue_size())
 
 def application(environ, start_response):
     response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
+    start_response('200 OK', response_headers)
     print "before defer", time.time()
     evwsgi.defer(toto, 0.2, False)
     #evwsgi.defer(toto, 1, True)
