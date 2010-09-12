58fdaf14b65bfc4921f7ac5ba0c87bfaa92e885d
William.os4y <william.os4y@gmail.com>
2010-09-12 22:01:22 +0200

    the hello sample should be just hello ;-)

--- sample/hello/hello_world.py
+++ sample/hello/hello_world.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import fapws._evwsgi as evwsgi
 from fapws import base
-import time
-import sys
-from fapws.contrib import views, zip, log
 
 def start():
     evwsgi.start("0.0.0.0", "8080")
@@ -14,55 +12,15 @@ def start():
         start_response('200 OK', [('Content-Type','text/html')])
         return ["hello world!!"]
 
-    @log.Log()
-    def staticlong(environ, start_response):
-        try:
-            f=open("long.txt", "rb")
-        except:
-            f=["Page not found"]
-        start_response('200 OK', [('Content-Type','text/html')])
-        return f
-    def embedlong(environ, start_response):
-        try:
-            c=open("long.txt", "rb").read()
-        except:
-            c=["Page not found"]
-        start_response('200 OK', [('Content-Type','text/html')])
-        return base.split_len(c,32768)
-    def staticshort(environ, start_response):
-        f=open("short.txt", "rb")
-        start_response('200 OK', [('Content-Type','text/html')])
-        return f
-    def testpost(environ, start_response):
-        print environ
-        print "INPUT DATA",environ["wsgi.input"].getvalue()
-        return ["OK. params are:%s" % (environ["fapws.params"])]
-
-    @log.Log()
-    @zip.Gzip()    
-    def staticlongzipped(environ, start_response):
-        try:
-            f=open("long.txt", "rb")
-        except:
-            f=["Page not found"]
-        start_response('200 OK', [('Content-Type','text/html')])
-        return f
     def iteration(environ, start_response):
         start_response('200 OK', [('Content-Type','text/plain')])
         yield "hello"
         yield " "
-        yield "worlds!!"
+        yield "world!!"
 
     
     evwsgi.wsgi_cb(("/hello", hello))
     evwsgi.wsgi_cb(("/iterhello", iteration))
-    evwsgi.wsgi_cb(("/longzipped", staticlongzipped))
-    evwsgi.wsgi_cb(("/long", staticlong))
-    evwsgi.wsgi_cb(("/elong", embedlong))
-    evwsgi.wsgi_cb(("/short", staticshort))
-    staticform=views.Staticfile("test.html")
-    evwsgi.wsgi_cb(("/staticform", staticform))
-    evwsgi.wsgi_cb(("/testpost", testpost))
 
     evwsgi.set_debug(0)    
     evwsgi.run()
