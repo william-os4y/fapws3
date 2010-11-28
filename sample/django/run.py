ca82e93ca03e78e79e2eb59938eeadfa678801d5
William.os4y <william.os4y@gmail.com>
2010-11-28 17:43:15 +0100

    Improvement proposed by Maxim Oransky

--- sample/django/run.py
+++ sample/django/run.py
@@ -1,29 +1,42 @@
 #!/usr/bin/env python
+from optparse import OptionParser
+parser = OptionParser()
+parser.set_defaults(
+    port='8000',
+    host='127.0.0.1',
+    settings='settings',
+)
+
+parser.add_option('--port', dest='port')
+parser.add_option('--host', dest='host')
+parser.add_option('--settings', dest='settings')
+parser.add_option('--pythonpath', dest='pythonpath')
+
+options, args = parser.parse_args()
+
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = options.settings
 
 import fapws._evwsgi as evwsgi
 from fapws import base
 import time
 import sys
-sys.setcheckinterval(100000) # since we don't use threads, internal checks are no more required
+sys.setcheckinterval=100000 # since we don't use threads, internal checks are no more required
+
+if options.pythonpath:
+    sys.path.insert(1, options.pythonpath)
+
 from fapws.contrib import django_handler, views
 import django
 
-def start():
-    evwsgi.start("0.0.0.0", "8080")
-    
-    evwsgi.set_base_module(base)
-    
-    def generic(environ, start_response):
-        res=django_handler.handler(environ, start_response)
-        return [res]
-    
-    mediafile=views.Staticfile(django.__path__[0] + '/contrib/admin/media/', maxage=2629000)
-    evwsgi.wsgi_cb(("/media/",mediafile))    
-    evwsgi.wsgi_cb(('',generic))
-    
-    evwsgi.set_debug(0)
-    evwsgi.run()
-    
+print 'start on', (options.host, options.port)
+evwsgi.start(options.host, options.port)
+evwsgi.set_base_module(base)
 
-if __name__=="__main__":
-    start()
+def generic(environ, start_response):
+    res=django_handler.handler(environ, start_response)
+    return [res]
+
+evwsgi.wsgi_cb(('',generic))
+evwsgi.set_debug(0)
+evwsgi.run()
