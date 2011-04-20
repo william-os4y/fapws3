f53d7108cf1ff1c8f22d07f2c73f86b94256e50b
William.os4y <william.os4y@gmail.com>
2011-04-20 17:06:26 +0200

    several changes in setup.py
    setup will now look for the place of libev.a hoping that libev.so or libev.dylib reside in the same folder
    the LIBPATH environment variable has been added. This is just to simplify definition of "lib" and "include" paths.
    Added Netbsd paths

--- setup.py
+++ setup.py
@@ -55,10 +55,15 @@ if os.environ.has_key('C_INCLUDE_PATH'):
     search_include_dirs.extend(os.environ['C_INCLUDE_PATH'].split(os.pathsep))
 if os.environ.has_key('LD_LIBRARY_PATH'):
     search_library_dirs.extend(os.environ['LD_LIBRARY_PATH'].split(os.pathsep))
+if os.environ.has_key('LIBPATH'):
+    search_library_dirs.append(os.path.join(os.environ['LIBPATH'],'lib'))
+    search_include_dirs.append(os.path.join(os.environ['LIBPATH'],'include'))
+
+
 
 #anyhow we include the standards directories
-search_library_dirs.extend(['/usr/lib','/usr/local/lib','/opt/local/lib','/usr/lib64'])
-search_include_dirs.extend(['/usr/include','/usr/local/include','/opt/local/include'])
+search_library_dirs.extend(['/usr/lib','/usr/local/lib','/opt/local/lib','/usr/lib64','/usr/pkg/lib/ev'])
+search_include_dirs.extend(['/usr/include','/usr/local/include','/opt/local/include','/usr/pkg/include/ev'])
 
 version=platform.python_version_tuple()
 if int(version[0])==2 and int(version[1])>=4:
@@ -81,13 +86,13 @@ if res==False:
     print "Please install the sources of python, or provide the path by setting the shell environmental variable C_INCLUDE_PATH"
     sys.exit(1)
 include_dirs.append(res)
-res=find_file('libev.so',search_library_dirs)
+res=find_file('libev.a',search_library_dirs)
 if res==False:
     print "We don't find 'libev.so' which is a mandatory file to run Fapws"
     print "Please install libev, or provide the path by setting the shell environmental variable LD_LIBRARY_PATH"
     sys.exit(1)
 library_dirs.append(res)
-
+extra_link_args=['-Wl,-R%s' % res]
 
 
 setup(name='fapws3',
@@ -115,8 +120,9 @@ classifiers=['Development Status :: 4 - Beta','Environment :: Web Environment','
                   depends=['fapws/extra.h','fapws/wsgi.h','fapws/mainloop.h','fapws/common.h'],
                   sources=['fapws/extra.c', 'fapws/wsgi.c','fapws/mainloop.c','fapws/_evwsgi.c'],
                   include_dirs=include_dirs,
-                  library_dirs=library_dirs, # add LD_RUN_PATH in your environment
+                  library_dirs=library_dirs, 
                   libraries=['ev'],
+                  extra_link_args=extra_link_args, #comment this line if you prefer to play with LD_LIBRARY_PATH
                   #extra_compile_args=["-ggdb"],
                   #define_macros=[("DEBUG", "1")],
                   )
