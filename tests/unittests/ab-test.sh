2601f63a3a1c8e30126ddff2a2d1dcb742d796d7
William.os4y <william.os4y@gmail.com>
2011-12-04 15:06:23 +0100

    Added some comments in the ab-test script

--- tests/unittests/ab-test.sh
+++ tests/unittests/ab-test.sh
@@ -2,11 +2,13 @@
 
 wait_sockets () {
 echo " ############## sleeping ..."
-sleep 60
+sleep 60  #This could be adapted
 echo "Open sockets: `netstat -a  | wc -l`"
 echo "################"
 }
-
+#
+#This bench is based on the famous HelloWorld provide by the unittests/server.py
+#
 nice -20 ab -n10000 -c1 http://127.0.0.1:8080/hello 
 wait_sockets
 nice -20 ab -n10000 -c2 http://127.0.0.1:8080/hello 
