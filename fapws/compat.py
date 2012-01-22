fe9021b69a9912af369fd45bfba2c4c69eaba696
William.os4y <william.os4y@gmail.com>
2012-01-22 11:24:38 +0100

    add the convert_to_bytes in git

---
+++ fapws/compat.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright (C) 2009 William.os4y@gmail.com
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 2 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+def convert_to_bytes(data):
+    if type(data)==bytes:   
+        return data
+    if type(data)==str:
+        return bytes(data,'utf8')
+    else:
+        conv = str(data)
+        return bytes(conv,'utf8')
+
