6e8bb15a5f1326616a1a14e030c863ebc212c75b
William.os4y <william.os4y@gmail.com>
2012-01-29 18:06:45 +0100

    fixes3 in sample_hello_world.py

--- sample/hello/hello_world.py
+++ sample/hello/hello_world.py
@@ -9,20 +9,21 @@ def start():
     evwsgi.set_base_module(base)
     
     def hello(environ, start_response):
+        print(environ)
         start_response('200 OK', [('Content-Type','text/html')])
         return [b"hello world!!"]
 
     def iteration(environ, start_response):
         start_response('200 OK', [('Content-Type','text/plain')])
-        yield "hello"
-        yield " "
-        yield "world!!"
+        yield b"hello"
+        yield b" "
+        yield b"world!!"
 
     
     evwsgi.wsgi_cb(("/hello", hello))
     evwsgi.wsgi_cb(("/iterhello", iteration))
 
-    evwsgi.set_debug(1)    
+    evwsgi.set_debug(0)    
     evwsgi.run()
     
 
