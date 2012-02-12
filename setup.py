c202fba8e7319ab3c7db44e8f11f3ade5f5a493c
William.os4y <william.os4y@gmail.com>
2012-02-12 12:12:34 +0100

    Improve setup.py

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ setup(name='fapws3',
       version="0.10",
       description="Fast Asynchronous Python Web Server",
       long_description=readme,
-classifiers=['Development Status :: 4 - Beta','Environment :: Web Environment','License :: OSI Approved :: GNU General Public License (GPL)','Programming Language :: C','Programming Language :: Python','Topic :: Internet :: WWW/HTTP :: HTTP Servers','Topic :: Internet :: WWW/HTTP :: WSGI :: Server'], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+classifiers=['Development Status :: 4 - Beta','Environment :: Web Environment','License :: OSI Approved :: GNU General Public License (GPL)','Programming Language :: C','Programming Language :: Python :: 2.4','Programming Language :: Python :: 2.5','Programming Language :: Python :: 2.6','Programming Language :: Python :: 2.7','Topic :: Internet :: WWW/HTTP :: HTTP Servers','Topic :: Internet :: WWW/HTTP :: WSGI :: Server'], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author='William',
       author_email='william.os4y@gmail.com',
